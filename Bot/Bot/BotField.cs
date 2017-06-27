3314bc942084ba3ca4597f651f72a0e52b87b5ae
Jose Karol Tumulak <tumulakjkm@gmail.com>
2017-06-27 20:25:35 +0800

    initialized project

---
+++ Bot/Bot/BotField.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bot
+{
+    public partial class BotField : Form
+    {
+        public BotField()
+        {
+            InitializeComponent();
+        }
+
+        private void BotField_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
