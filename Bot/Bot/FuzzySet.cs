c97a5aebe7cc444558288015f05ceb7dc96b7865
jambrad123 <jambrad123@gmail.com>
2017-07-20 23:56:25 +0800

    Added Set and Membership function in FuzzySet

--- Bot/Bot/FuzzySet.cs
+++ Bot/Bot/FuzzySet.cs
@@ -30,13 +30,81 @@ namespace Bot
             double x2, double y2, double x3, double y3)
         {
             //Temporary holders
-            double[] tempX;
-            double[] tempY;
-
+            double[] tempX = new double[4];
+            double[] tempY = new double[4];
+            this.index = index;
             this.name = name;
+            tempX[0] = x0;
+            tempY[0] = y0;
+            this.n = 1;
 
+            if ((x1 != x0) || (y1 != y0))
+            {
+                tempX[n] = x1;
+                tempY[n] = y1;
+                ++n;
+            }
+
+            if ((x2 != x1) || (y2 != y1))
+            {
+                tempX[n] = x2;
+                tempY[n] = y2;
+                ++n;
+            }
+
+            if ((x3 != x2) || (y3 != y2))
+            {
+                tempX[n] = x3;
+                tempY[n] = y3;
+                ++n;
+            }
+
+            x = new double[n];
+            y = new double[n];
+
+            if (x == null)
+            {
+                n = 0;
+                return;
+            }
+
+            tempX.CopyTo(x, 0);
+            tempY.CopyTo(y, 0);
             
         }
-        
+
+        public double membership(double point)
+        {
+            int low, mid, high;
+            double tempY;
+
+            if (n == 0)
+            {
+                return 0;
+            }
+
+            if (point <= x[0])
+                return y[0];
+
+            if (point >= x[n - 1])
+                return y[n - 1];
+
+            low = 0;
+            high = n - 1;
+
+            for (; ; )
+            {
+                mid = (low + high) / 2;
+                if (mid == low)
+                    break;
+                if (x[mid] < point)
+                    low = mid;
+                else
+                    high = mid;
+            }
+
+            tempY = (point - x[high - 1]) / (x[high] - x[high - 1]) * (y[high] - y[high - 1]);
+            return tempY + y[high - 1];
+        }
     }
 }
