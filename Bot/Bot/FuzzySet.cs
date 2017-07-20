c34858628a2b118c9be1f3b8fae829b27e281a79
jambrad123 <jambrad123@gmail.com>
2017-07-20 13:21:00 +0800

    Added constructor in fuzzySet

--- Bot/Bot/FuzzySet.cs
+++ Bot/Bot/FuzzySet.cs
@@ -9,9 +9,34 @@ namespace Bot
     //Class holding the details of a FuzzySet
     class FuzzySet
     {
-      
+        //Linguistic Name
         private String name {get; set;}
+        //Linguistic Index
         private int index { get; set; }
+        //Number of points in the function
+        private int n { get; set; }
+        //Array of the x-point values
+        private double[] x;
+        //Array of the degree of membership of the x-point values
+        private double[] y;
+
+        public FuzzySet(){
+            n = 0;
+            x = null;
+            y = null;
+        }
+
+        public void Set(String name, int index, double x0, double y0, double x1, double y1,
+            double x2, double y2, double x3, double y3)
+        {
+            //Temporary holders
+            double[] tempX;
+            double[] tempY;
+
+            this.name = name;
+
+            
+        }
         
     }
 }
